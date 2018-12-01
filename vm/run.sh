480fc0e3b772ad14361cf4a6c728c7490a0108bf
KentFujii <kent.where.the.light.is@gmail.com>
2018-12-01 23:19:15 +0900

    wip: vm

--- vm/run.sh
+++ vm/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd `dirname $0`
-packer build packer/web.json
-# packer build packer/app.json
-# packer build packer/db.json
+packer build packer/db.json
 # docker run -p 8080:8080 -it gcp_iac_vm_app /bin/bash -c "cd /app && python app.py"
-docker run -p 80:80 -it gcp_iac_vm_web nginx
