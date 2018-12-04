0a75b44b0c3bf72e80523d5fc1f88475e11c69a4
KentFujii <kent.where.the.light.is@gmail.com>
2018-12-05 00:32:33 +0900

    wip: docker_registory

--- vm/run.sh
+++ vm/run.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 cd `dirname $0`
 
+# prepare registory
+docker pull registry
+docker run -d -p 5000:5000 registry
+
+
 # create image
-# (
-#   cd packer
-#   packer build app.json
-# )
+(
+  cd packer
+  packer build app.json
+)
+
+# push image
+docker push localhost:5000/gcp_iac_vm_app
 
 # orchestrate image
-(
-  cd terraform
-  terraform init
-  terraform apply -auto-approve
-)
+# (
+#   cd terraform
+#   terraform init
+#   terraform apply -auto-approve
+# )
 # docker run -p 8080:8080 -it gcp_iac_vm_app /bin/bash -c "cd /app && python app.py"
 # docker run --name mysql -e MYSQL_ROOT_PASSWORD=mysql -d -p 3306:3306 mysql
--- vm/terraform/main.tf
+++ vm/terraform/main.tf
@@ -2,8 +2,15 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-resource "docker_image" "mysql" {
-  name = "mysql:latest"
+resource "docker_container" "app" {
+  image = "gcp_iac_vm_app"
+  name  = "app"
+  hostname = "app"
+  ports {
+    internal = 8080
+    external = 8080
+  }
+  command = ["cd /app && python app.py"]
 }
 
 resource "docker_container" "mysql" {
@@ -19,4 +26,8 @@ resource "docker_container" "mysql" {
     volume_name = "${path.module}/../db/"
     container_path = "/docker-entrypoint-initdb.d/"
   }
+}
+
+resource "docker_image" "mysql" {
+  name = "mysql:latest"
 }
